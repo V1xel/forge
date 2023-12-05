da63a6600ec3611b94f1fd4aef9a2e26f95391bb
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-05 20:58:18 +0100

    Updated names in combine vector3

--- src/domain/vector/combineVector3.ts
+++ src/domain/vector/combineVector3.ts
@@ -2,12 +2,12 @@ import { IValueType } from "../valueType";
 import { Vector3 } from "./vector3";
 
 export class CombineVector3 {
-    constructor(private x: IValueType, private y: IValueType, private z: IValueType) { }
+    constructor(private _x: IValueType, private _y: IValueType, private _z: IValueType) { }
 
     public getResult(): IValueType {
-        const x = this.x.getScalar().getValue()
-        const y = this.y.getScalar().getValue()
-        const z = this.z.getScalar().getValue()
+        const x = this._x.getScalar().getValue()
+        const y = this._y.getScalar().getValue()
+        const z = this._z.getScalar().getValue()
 
         return new Vector3(x, y, z)
     }
