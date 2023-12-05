dc7febe0b1d418e50662c9c23fba29ae979fff19
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-05 22:03:56 +0100

    Added vector3 operation node

---
+++ src/nodes/vector3Operation/vector3OperationNode.tsx
@@ -0,0 +1,43 @@
+import { ComponentType, useEffect, useState } from "react"
+import { Card } from "react-bootstrap"
+import { NodeData } from '../../state/store'
+import { Scalar } from "../../domain/scalar/scalar"
+import { IValueType, ValueType } from "../../domain/valueType"
+import { NodeProps } from "reactflow"
+import { InputCollection, OutputCollection } from "../components/collection"
+import { ScalarOperationType } from "../../domain/scalar/scalarOperation"
+import { Select } from "../components/select"
+import { Vector3Operation, Vector3OperationType } from "../../domain/vector/vectorOperation"
+
+export const Vector3OperationNode: ComponentType<NodeProps<NodeData>> = ({ id }) => {
+    const [vector3A, setVector3A] = useState(new Scalar(0) as IValueType)
+    const [vector3B, setVector3B] = useState(new Scalar(0) as IValueType)
+    const [type, setType] = useState(Vector3OperationType.Add)
+    const [result, setResult] = useState(new Scalar(0) as IValueType)
+
+    const vector3Operation = new Vector3Operation(vector3A, vector3B, type)
+    useEffect(() => { setResult(vector3Operation.getResult()) }, [vector3A, vector3B, type])
+
+    const setInputA = (value: IValueType) => { setVector3A(value) }
+    const setInputB = (value: IValueType) => { setVector3B(value) }
+    const setOperationType = (value: string) => { setType(value as Vector3OperationType) }
+
+    return (
+        <Card data-bs-theme={'dark'} className="scalar-node">
+            <Card.Header className="scalar-node-header">Vector3 Operation</Card.Header>
+            <Card.Body>
+                <OutputCollection
+                    nodeId={id}
+                    elements={[
+                        { title: 'Result', type: ValueType.Scalar, result: result },
+                    ]}
+                />
+                <Select onChange={setOperationType} enumType={Vector3OperationType} />
+                <InputCollection nodeId={id} elements={[
+                    { type: ValueType.Vector3, hideHandle: false, onChange: setInputA },
+                    { type: ValueType.Vector3, hideHandle: false, onChange: setInputB }
+                ]} />
+            </Card.Body>
+        </Card>
+    )
+}
