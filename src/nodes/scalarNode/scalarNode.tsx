734597d207fb1f2f30961ea440b9fb5e9dd57a93
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-05 16:45:32 +0100

    Formatted scalar node

--- src/nodes/scalarNode/scalarNode.tsx
+++ src/nodes/scalarNode/scalarNode.tsx
@@ -1,11 +1,11 @@
-import { ComponentType, useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
-import { NodeData, useStore } from '../../state/store';
-import { Scalar } from "../../domain/scalar/scalar";
-import { IValueType, ValueType } from "../../domain/valueType";
-import { NodeProps } from "reactflow";
-import { Output } from "../components/output";
-import { InputCollection } from "../components/collection";
+import { ComponentType, useEffect, useState } from "react"
+import { Card } from "react-bootstrap"
+import { NodeData, useStore } from '../../state/store'
+import { Scalar } from "../../domain/scalar/scalar"
+import { IValueType, ValueType } from "../../domain/valueType"
+import { NodeProps } from "reactflow"
+import { Output } from "../components/output"
+import { InputCollection } from "../components/collection"
 
 export const ScalarNode: ComponentType<NodeProps<NodeData>> = ({ id }) => {
     const [scalar, setScalar] = useState(new Scalar(0) as IValueType)
