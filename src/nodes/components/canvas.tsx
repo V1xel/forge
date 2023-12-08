c2eeef30053adbcddcd2421b8424d8857f22d3e1
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-08 20:01:33 +0100

    Updated canvas component

--- src/nodes/components/canvas.tsx
+++ src/nodes/components/canvas.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react"
-import { WebGPUEngine } from '../../webGPU/engine'
 import { IDataType } from "../../domain/dataType"
+import { WebGPUCanvas } from "../../webGPU2/wrappers/canvas"
+import { WebGPUEngine } from "../../webGPU2/engine"
+import { Matrix4 } from "../../domain/matrix/matrix4"
+import { Vector4 } from "../../domain/vector/vector4"
 import { Geometry } from "../../domain/geometry"
-import { WebGPUCanvas, WebGPUDevice } from "../../webGPU/canvas"
 import { Vector3 } from "../../domain/vector/vector3"
 
 export interface ICanvasProps {
@@ -17,12 +19,10 @@ const MakeCanvasId = (nodeId: string) => `canvas-nodeId:${nodeId}`
 export const Canvas = ({ nodeId, geometry, color, shader }: ICanvasProps) => {
     useEffect(() => {
         const canvas = WebGPUCanvas.getCanvas(MakeCanvasId(nodeId))
-        const engine = new WebGPUEngine(WebGPUDevice.device, WebGPUDevice.format, geometry as Geometry)
-        const frame = engine.init(shader, (color as Vector3).getVector3().getValue())
-        engine.bindContext(canvas)
-        
-        const animationFrameId = frame()
-        return () => cancelAnimationFrame(animationFrameId)
+        const engine = new WebGPUEngine(canvas)
+        engine.initialize(geometry as Geometry, shader, { color: color as Vector3, transform: new Matrix4(new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0), new Vector4(0, 0, 0, 0)) })
+
+        return engine.loop()
     }, [nodeId, geometry, color])
 
     return <canvas id={MakeCanvasId(nodeId)} height={280} width={280}></canvas>
